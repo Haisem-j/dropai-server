a6c6576524612e74410cd01e2d5abb3dacb5b069
Haisem Jemal <haisemjemal@Haisems-MacBook-Air.local>
2022-09-28 13:33:06 -0400

    landing page links to app

--- config/firebase-config.ts
+++ config/firebase-config.ts
@@ -1,9 +1,10 @@
 import admin from "firebase-admin";
+import path from "path";
 import { getFirestore } from "firebase-admin/firestore";
 
 // Include absolute path to serviceAccoutKey.json
 
-const serviceAccountKey = require("./serviceAccountKey.json");
+const serviceAccountKey = require("../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
